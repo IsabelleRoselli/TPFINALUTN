4a59d46e75a783fd88842284a9f3b5c8b4cd3522
Isabelle Roselli <isa.roselli@hotmail.com>
2026-05-25 06:13:56 -0300

    icon

--- backend/app.js
+++ backend/app.js
@@ -25,6 +25,7 @@ app.use((req, res, next) => {
   }
   next();
 });
+app.use('/images', express.static(path.join(rootDir, 'images')));
 
 // ===== FRONTEND ESTÁTICO =====
 const rootDir = path.join(__dirname, "..");
